3a3d75d67c4f599fd10f33adf3c171e0b1654026
Hans Yulian <hansyulian@windowslive.com>
2025-09-02 10:48:59 +0700

    ganti greeting

--- app/src/main/java/git/App.java
+++ app/src/main/java/git/App.java
@@ -5,7 +5,7 @@ package git;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello Object Oriented Programming!";
     }
 
     public static void main(String[] args) {
