f6f1fd99be74a7e99fdbcb17b8a026bed8238270
MrSushi74 <jameskoh313@gmail.com>
2025-09-02 11:10:42 +0700

    tambah james

---
+++ app/src/main/java/git/james.java
@@ -0,0 +1,10 @@
+package git;
+
+public class james {
+    public String getGreeting (){
+        
+    }
+    public static void main(String[] args){
+        System.out.println("Hi from James");
+    }
+}
