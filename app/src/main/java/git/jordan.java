b47b96344d1ba0ef8bbc03bb95d3bb1d577784ca
Jordan Alexander Suwito <jordansuwito@gmail.com>
2025-09-02 11:06:50 +0700

    tambah jordan

--- app/src/main/java/git/jordan.java
+++ app/src/main/java/git/jordan.java
@@ -2,6 +2,6 @@ package git;
 
 public class jordan {
     public static void main(String[] args) {
-        System.out.println("Hello Jordan Suwito");
+        System.out.println("Hello Jordan Suwito!");
     }
 }
